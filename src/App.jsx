98823f2a161527b42f50e66fdbc962c2cb4a5c65
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-04-17 11:42:04 -0300

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./styles/global.css"
+
+
+function App() {
+  return (
+    <div className="bg-black w-screen h-screen">
+      <div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
